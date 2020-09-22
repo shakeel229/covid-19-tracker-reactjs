eea1ae520e91ef2cab9a1b428d16d803f0a7f7b8
Shakeel-Anjum <shakeel229@gmail.com>
2020-09-22 11:58:53 +1000

    countries dropdown menu added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries2 = data.map((country) => {
+            return { name: country.country, value: country.countryInfo.iso2 };
+          });
+          setCountries(countries2);
+          console.log(countries2);
+        });
+    };
+    getCountriesData();
+  }, []);
   return (
     <div className="App">
-      <h1>Covid tracker app by shakeel 229</h1>
+      <div className="app_header">
+        <h1>COVID 19 TRACKER</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">worldwide</MenuItem>
+            {countries.map((country) => {
+              return <MenuItem value={country.value}>{country.name}</MenuItem>;
+            })}
+            <MenuItem value="worldwide">worldwide</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
